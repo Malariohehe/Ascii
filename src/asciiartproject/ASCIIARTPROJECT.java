79e45f74e42e1608a74512d2e362ee652690114d
Keyin Altman <20655@my4county.net>
2021-08-27 10:40:45 -0400

    made my art

--- src/asciiartproject/ASCIIARTPROJECT.java
+++ src/asciiartproject/ASCIIARTPROJECT.java
@@ -16,7 +16,20 @@ public class ASCIIARTPROJECT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+       
+        System.out.println("======:========;()");
+        System.out.println("      ######::::");
+        System.out.println("      ######::::");
+        System.out.println("      ######::::");
+        System.out.println("      ######::::");
+        System.out.println("      ##########");
+        System.out.println("      ##########");
+        System.out.println("      ##########");
+        System.out.println("      ##########");
+        
+        
+        
+        
         // TODO code application logic here
     }
         
